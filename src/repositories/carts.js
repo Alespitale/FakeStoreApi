0e422bd498b975ec5cf0fcc3dcbeceaaeaf85569
Alespitale <alespitale6@gmail.com>
2022-08-22 22:01:11 -0300

    repositories

---
+++ src/repositories/carts.js
@@ -0,0 +1,11 @@
+export class CartsRepository {
+    constructor(cartsModel) {
+        this.cartsModel = cartsModel
+    }
+    async getAll() {
+        return await this.cartsModel.getAll()
+    }
+    async getById(id) {
+        return await this.cartsModel.getById(id)
+    }
+}
---
+++ src/repositories/users.js
@@ -0,0 +1,11 @@
+export class UsersRepository {
+    constructor(usersModel) {
+        this.usersModel = usersModel
+    }
+    async getAll() {
+        return await this.usersModel.getAll()
+    }
+    async getById(id) {
+        return await this.usersModel.getById(id)
+    }
+}
